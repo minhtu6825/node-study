b6e0dfeba0e66ae452dd87efd286dda42a3a5911
tu <minhtu6825@gmail.com>
2022-10-25 14:23:03 +0700

    test return axio json data

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var http = require('http');
 const axios = require('axios');
 console.log(1);
 axios.get('https://api.jsonbin.io/v3/qs/63578b8b0e6a79321e33bf13').then(
-  res => {
-    console.log('res:', res.data);
+  res1 => {
+    console.log('res:', res1.data);
     http.createServer(function (req, res) {
       console.log(`Just got a request at ${req.url}!`)
-      res.write(JSON.stringify(res.data));
+      res.write(JSON.stringify(res1.data));
       res.end();
-    }).listen(process.env.PORT || 3000);
+    }).listen(process.env.PORT || 3001);
   }
 ).catch(
   err => {
