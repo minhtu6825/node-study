44975bf5111ccfa805ac52678ecd118322fa0c02
tu <minhtu6825@gmail.com>
2022-10-25 14:12:29 +0700

    test axios

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var http = require('http');
-
+const axios = require('axios');
+console.log(1);
+axios.get('https://api.jsonbin.io/v3/qs/63578b8b0e6a79321e33bf13').then(
+  res => {
+    console.log('res:', res.data);
+  }
+).catch(
+  err => {
+    console.log('err:', err);
+  }
+).finally(
+  console.log('request by axios done')
+)
+console.log(2);
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
+  console.log(`Just got a request at ${req.url}!`)
+  res.write('Yo!');
+  res.end();
 }).listen(process.env.PORT || 3000);
